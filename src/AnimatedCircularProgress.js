2d985c191d7e8f94728b0d1e6ed9d151ffa17f14
Ali-Ayyad <ali.j.ayyad@gmail.com>
2017-11-16 18:05:17 +0200

    Missed AppState

--- src/AnimatedCircularProgress.js
+++ src/AnimatedCircularProgress.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, Animated } from 'react-native';
+import { View, Animated, AppState } from 'react-native';
 import CircularProgress from './CircularProgress';
 const AnimatedProgress = Animated.createAnimatedComponent(CircularProgress);
 
